d37021c9c89e0b2c7e8a4c4b410683868c47b7c0
meena7800 <rajeshmeenassiti@gmail.com>
2025-12-01 14:17:13 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Use (process as any) to bypass TypeScript checking for 'cwd' on some systems
+  const env = loadEnv(mode, (process as any).cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  }
+})
